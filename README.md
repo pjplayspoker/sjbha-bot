a683fa3c5a89acb66f13e4101fa9d96b2657a738
Seb <4062679+hellos3b@users.noreply.github.com>
2021-08-05 12:47:09 -0700

    :sparkles: Setup frontend gh-pages pipeline

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-Readme WIP
+## Basic Guide
+
+More readme coming someday, but for now
+
+#### Running the bot
+
+You will need a `.env` file in the root of `packages/backend`
+
+You can look at [env.ts](./packages/backend/src/app/env.ts) for what variables are expected
+
+```sh
+> npm i
+> cd packages/backend
+> npm i
+> npm run dev
+
+# If working on something that uses the database,
+# you also need to start the db
+> npm run db
+```
--- packages/frontend/webpack.config.js
+++ packages/frontend/webpack.config.js
@@ -1,43 +1,55 @@
-const path = require("path");
-const HTMLPlugin = require("html-webpack-plugin");
+const path = require ('path');
+const HTMLPlugin = require ('html-webpack-plugin');
 
-const __www = path.resolve(__dirname, "webpack")
 
-// Webpack config
-module.exports = {
-  entry: path.resolve(__dirname, '@frontend/router.ts'),
-  devtool: "inline-source-map",
+module.exports = (_, argv) => ({
+  entry:   './src/index.tsx',
+  devtool: 
+    (argv.mode === 'development') 
+      ? 'inline-source-map'
+      : undefined,
   
   output: {
-    path: path.resolve(__dirname, 'public'),
-    publicPath: "/",
-    filename: '[name].js'
+    path:       path.resolve (__dirname, 'build'),
+    publicPath: './',
+    clean:      true,
+    filename:   '[name].bundle.js'
   },
 
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+
+  mode: argv.mode,
+
   plugins: [
-    new HTMLPlugin()
+    new HTMLPlugin ({
+      template: 'src/index.html'
+    })
   ],
 
   module: {
     rules: [
       {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
+        test:    /\.tsx?$/,
+        use:     'awesome-typescript-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.md$/i,
-        use: 'raw-loader',
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource'
       }
     ]
   },
 
   resolve: {
-    extensions: ['.ts', '.tsx']
+    extensions: ['.ts', '.tsx', '.js']
   }
   
-}
+});
